2d3e541203e41cfeb89b423e882c096ae9eafd30
hxhdj gfjdke <evgeniyzhavoronok@gmail.com>
2022-01-06 12:20:48 +0300

    add get routes

--- server/index.js
+++ server/index.js
@@ -9,17 +9,38 @@ app.use(express.json());
 
 //ROUTES
 
-app.post("/todos", (req, res) => {
+app.post("/todos", async (req, res) => {
     try {
         const { description } = req.body;
         const newTodo = await pool.query(
             "INSERT INTO todo (description) VALUES($1) RETURNING *",
             [description]
         );
+        res.json(newTodo.rows[0]);
     } catch (err) {
         console.error(err.message);
     }
 });
+
+app.get("/todos", async (req, res) => {
+    try {
+        const allTodos = await pool.query("SELECT * FROM todo");
+        res.json(allTodos.rows);
+    } catch (err) {
+        console.error(err.message);
+    }
+});
+
+app.get("/todos/:id", async (req, res) => {
+    try {
+        const { id } = req.params;
+        const todo = await pool.query("SELECT * FROM todo WHERE todo_id = $1", [id]);
+
+        res.json(todo.rows[0]);
+    } catch (err) {
+        console.error(err.messge);
+    }
+})
 
 app.listen(5000, () => {
     console.log("server has started on port 5000");
