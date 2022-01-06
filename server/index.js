14063eb6b37f74e0d065d5714be31ba59bf85dd6
hxhdj gfjdke <evgeniyzhavoronok@gmail.com>
2022-01-06 11:55:31 +0300

    add post route

--- server/index.js
+++ server/index.js
@@ -9,6 +9,18 @@ app.use(express.json());
 
 //ROUTES
 
+app.post("/todos", (req, res) => {
+    try {
+        const { description } = req.body;
+        const newTodo = await pool.query(
+            "INSERT INTO todo (description) VALUES($1) RETURNING *",
+            [description]
+        );
+    } catch (err) {
+        console.error(err.message);
+    }
+});
+
 app.listen(5000, () => {
     console.log("server has started on port 5000");
 })
